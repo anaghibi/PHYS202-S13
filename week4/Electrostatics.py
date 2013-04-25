37792e996019c994e6ba793dc6ea039e3983b60b
anaghibi <anaghibi@calpoly.edu>
2013-04-25 13:59:59 -0700

    updating classwork from Thursday of Week4

---
+++ week4/Electrostatics.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def pointPotential(x,y,q,posx,posy):
+    '''return electric potential for a point particle at (posx,posy)'''
+    k = 8.98755*10**9
+    Vpoint = (k*q/((x-posx)**2+(y-posy)**2)**(1/2.))
+    return Vpoint
+
+def dipolePotential(x,y,q1,q2,d):
+    '''return electric potential for a dipole'''
+    k = 8.98755*10**9
+    Vdipole = (k*q1/(x**2+(y-(d/2))**2)**(1/2.))-(k*q2/(x**2+(y+(d/2))**2)**(1/2.))
+    return Vdipole
