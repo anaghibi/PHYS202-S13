046677dc36c702b4cac1e5c11e305551478c0d4a
anaghibi <anaghibi@calpoly.edu>
2013-05-14 14:09:33 -0700

    Tues work

--- week4/Electrostatics.py
+++ week4/Electrostatics.py
@@ -6,8 +6,8 @@ def pointPotential(x,y,q,posx,posy):
     Vpoint = (k*q/((x-posx)**2+(y-posy)**2)**(1/2.))
     return Vpoint
 
-def dipolePotential(x,y,q1,q2,d):
+def dipolePotential(x,y,q,d):
     '''return electric potential for a dipole'''
     k = 8.98755*10**9
-    Vdipole = (k*q1/(x**2+(y-(d/2))**2)**(1/2.))-(k*q2/(x**2+(y+(d/2))**2)**(1/2.))
+    Vdipole = (k*q/(x**2+(y-d)**2)**(1/2.))-(k*q/(x**2+(y+d)**2)**(1/2.))
     return Vdipole
